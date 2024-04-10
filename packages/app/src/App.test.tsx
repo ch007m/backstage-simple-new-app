d5d6897a81f7a991a42521d773695638141238ad
cmoulliard <cmoulliard@redhat.com>
2024-04-10 17:46:58 +0200

    WIP. Added a test case which is not working
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- packages/app/src/App.test.tsx
+++ packages/app/src/App.test.tsx
@@ -1,35 +1,71 @@
-import { renderWithEffects } from '@backstage/test-utils';
+import {renderWithEffects} from '@backstage/test-utils';
+import {createSpecializedApp} from '@backstage/frontend-app-api';
+import {
+    createExtensionOverrides,
+    createPageExtension,
+} from "@backstage/frontend-plugin-api";
+import {render, screen} from "@testing-library/react";
+import React from 'react';
+import {ScaffolderPage} from "@backstage/plugin-scaffolder";
 
 // Rarely, and only in windows CI, do these tests take slightly more than the
 // default five seconds
 jest.setTimeout(15_000);
 
 describe('App', () => {
-  it('should render', async () => {
-    process.env = {
-      NODE_ENV: 'test',
-      APP_CONFIG: [
-        {
-          data: {
-            app: {
-              title: 'Test',
-              support: { url: 'http://localhost:7007/support' },
-            },
-            backend: { baseUrl: 'http://localhost:7007' },
-            lighthouse: {
-              baseUrl: 'http://localhost:3003',
-            },
-            techdocs: {
-              storageUrl: 'http://localhost:7007/api/techdocs/static/docs',
-            },
-          },
-          context: 'test',
-        },
-      ] as any,
-    };
+    it('should render', async () => {
+        process.env = {
+            NODE_ENV: 'test',
+            APP_CONFIG: [
+                {
+                    data: {
+                        app: {
+                            title: 'Test',
+                            support: {url: 'http://localhost:7007/support'},
+                        },
+                        backend: {baseUrl: 'http://localhost:7007'},
+                        lighthouse: {
+                            baseUrl: 'http://localhost:3003',
+                        },
+                        techdocs: {
+                            storageUrl: 'http://localhost:7007/api/techdocs/static/docs',
+                        },
+                    },
+                    context: 'test',
+                },
+            ] as any,
+        };
 
-    const { default: app } = await import('./App');
-    const rendered = await renderWithEffects(app);
-    expect(rendered.baseElement).toBeInTheDocument();
-  });
+        const {default: app} = await import('./App');
+        const rendered = await renderWithEffects(app);
+        expect(rendered.baseElement).toBeInTheDocument();
+    });
+
+    it('should work using createExtensionOverrides', async () => {
+        const app = createSpecializedApp({
+            features: [
+                createExtensionOverrides({
+                    extensions: [
+                        createPageExtension({
+                            namespace: 'scaffolder',
+                            defaultPath: '/my-create',
+                            loader: async () =>
+                                import('@qshift/plugin-quarkus').then(m =>
+                                    <ScaffolderPage/>
+/*                                    <div data-testid="field-content">
+                                        <m.QuarkusVersionListField/>
+                                    </div>*/
+                                ),
+                        }),
+                    ],
+                }),
+            ],
+        });
+
+        render(app.createRoot());
+
+        await expect(
+            screen.findByText('scaffolder'),
+        ).resolves.toBeInTheDocument();
+    });
 });
