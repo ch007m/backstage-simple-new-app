b50ae7ade044820794c7ec6c997451bacd5efde2
cmoulliard <cmoulliard@redhat.com>
2024-04-10 15:00:07 +0200

    Create a pageExtension to override the scaffolder page and pass some ScaffolderFieldExtensions
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import {
-    configApiRef, createApiFactory,
+    configApiRef,
+    createApiFactory,
     SignInPageProps,
 } from '@backstage/core-plugin-api';
 
@@ -10,6 +11,7 @@ import {
 } from '@backstage/core-components';
 
 import {
+    createPageExtension,
     createExtensionOverrides,
     createSignInPageExtension,
 } from '@backstage/frontend-plugin-api';
@@ -22,12 +24,28 @@ import {createApp} from '@backstage/frontend-app-api';
 import {ScmAuth, ScmIntegrationsApi, scmIntegrationsApiRef} from "@backstage/integration-react";
 import userSettingsPlugin from '@backstage/plugin-user-settings/alpha';
 import techdocsPlugin from "@backstage/plugin-techdocs/alpha";
+import scaffolderPlugin from "@backstage/plugin-scaffolder/alpha";
+import {convertLegacyApp, convertLegacyRouteRef} from "@backstage/core-compat-api";
+import {ScaffolderFieldExtensions} from "@backstage/plugin-scaffolder-react";
+import {ScaffolderPage} from "@backstage/plugin-scaffolder";
+import {FlatRoutes} from "@backstage/core-app-api";
+import {Route} from "react-router";
+import {QuarkusVersionListField} from "@qshift/plugin-quarkus";
 
+/*function ScaffolderFieldExtensionsPage() {
+    return (
+        <ScaffolderFieldExtensions>
+            <QuarkusVersionListField/>
+            <QuarkusExtensionListField/>
+            <QuarkusQuickstartPickerField/>
+        </ScaffolderFieldExtensions>
+    );
+};*/
 
 const signInPage = createSignInPageExtension({
     name: 'guest',
     loader: async () => (props: SignInPageProps) =>
-        <SignInPage {...props} providers={['guest']} />,
+        <SignInPage {...props} providers={['guest']}/>,
 });
 
 const scmAuthExtension = createApiExtension({
@@ -37,21 +55,65 @@ const scmAuthExtension = createApiExtension({
 const scmIntegrationApi = createApiExtension({
     factory: createApiFactory({
         api: scmIntegrationsApiRef,
-        deps: { configApi: configApiRef },
-        factory: ({ configApi }) => ScmIntegrationsApi.fromConfig(configApi),
+        deps: {configApi: configApiRef},
+        factory: ({configApi}) => ScmIntegrationsApi.fromConfig(configApi),
     }),
 });
+
+/*
+  /create appears within the left menu bar but if we click on the link, the screen is white
+  The home screen reports: ERROR 404: PAGE NOT FOUND
+  DevTool console don't report errors
+
+  If we wrap the tags <ScaffolderFieldExtensions> with <ScaffolderPage> then we got as error: App context is not available
+ */
+const scaffolderPage = createPageExtension({
+    namespace: 'scaffolder',
+    defaultPath: '/my-create',
+    // routeRef is mandatory otherwise we got: No path for routeRef{type=absolute,id=scaffolder}
+    routeRef: scaffolderPlugin.routes.root,
+    loader: () =>
+        import('@qshift/plugin-quarkus').then(m =>
+                <ScaffolderFieldExtensions>
+                    <m.QuarkusVersionListField/>
+                </ScaffolderFieldExtensions>
+        ),
+});
+
+/*
+   /create don't appear on the left menu bar when
+   we use this convertLegacyApp function and pass it as
+   features to:  createApp({
+   The home screen reports: ERROR 404: PAGE NOT FOUND
+ */
+const legacyPlugins = convertLegacyApp(
+    <FlatRoutes>
+        <Route
+            path="/create"
+            element={
+                <ScaffolderPage>
+                    <ScaffolderFieldExtensions>
+                        <QuarkusVersionListField/>
+                    </ScaffolderFieldExtensions>
+                </ScaffolderPage>
+            }
+        />
+    </FlatRoutes>,
+);
 
 
 const app = createApp({
     features: [
         userSettingsPlugin, /* Needed to see on the left menu bar the settings link */
         techdocsPlugin, /* Needed to compose, render techdocs, etc */
+        scaffolderPlugin,
+        //...legacyPlugins,
         createExtensionOverrides({
             extensions: [
                 signInPage,
                 scmIntegrationApi, /* Needed to scaffold a template */
                 scmAuthExtension, /* Needed to scaffold a template */
+                scaffolderPage,
             ],
         }),
     ],
